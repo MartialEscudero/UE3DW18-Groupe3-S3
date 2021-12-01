0715e3a570265f656527ea390f53de1f738e5a1b
Escudero Martial <martial.escudero@gmail.com>
2021-12-01 22:21:30 +0100

    add 4 new links

--- db/watson.sql
+++ db/watson.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : Dim 06 déc. 2020 à 05:08
--- Version du serveur :  10.4.11-MariaDB
--- Version de PHP : 7.4.3
+-- Hôte : localhost
+-- Généré le : mer. 01 déc. 2021 à 21:19
+-- Version du serveur : 8.0.26-0ubuntu0.20.04.2
+-- Version de PHP : 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `watson`
 --
-CREATE DATABASE IF NOT EXISTS `watson` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `watson`;
 
 -- --------------------------------------------------------
 
@@ -31,12 +28,12 @@ USE `watson`;
 --
 
 CREATE TABLE `tl_liens` (
-  `lien_id` int(11) NOT NULL,
-  `lien_url` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `lien_titre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `lien_desc` text COLLATE utf8_unicode_ci DEFAULT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `lien_id` int NOT NULL,
+  `lien_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `lien_titre` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `lien_desc` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+  `user_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `tl_liens`
@@ -45,7 +42,11 @@ CREATE TABLE `tl_liens` (
 INSERT INTO `tl_liens` (`lien_id`, `lien_url`, `lien_titre`, `lien_desc`, `user_id`) VALUES
 (1, 'https://cvtic.unilim.fr/', 'Campus Virtuel TIC', 'Site internet du CvTIC.', 1),
 (2, 'https://duckduckgo.com/', 'Duck Duck Go', 'Le moteur de recherche qui ne trace pas ses utilisateurs.', 1),
-(3, 'https://framasoft.org/', 'Framasoft', 'Un réseau dédié à la promotion du « libre » en général et du logiciel libre en particulier.', 1);
+(3, 'https://framasoft.org/', 'Framasoft', 'Un réseau dédié à la promotion du « libre » en général et du logiciel libre en particulier.', 1),
+(4, 'https://nextcloud.com/', 'Nextcloud', 'Nextcloud est un logiciel libre de site d\'hébergement de fichiers et une plateforme de collaboration.', 1),
+(5, 'https://1password.com/fr/', '1Password', '1Password est un gestionnaire de mots de passe et permet de générer et de conserver des mots de passe de façon sécurisée, ainsi que des notes et documents.', 1),
+(6, 'https://signal.org/fr/', 'Signal', 'Signal est une application gratuite pour Android et iOS, permettant de communiquer de façon chiffrée et sécurisée dont l\'objectif est d\'assurer un maximum de confidentialité à ses utilisateurs.', 1),
+(7, 'https://www.notion.so/product', 'Notion', 'Notion est une application de prise de notes, de bases de données, des tableaux Kanban, de wikis, de calendriers et de rappels. C\'est à la fois un idéateur et un wiki. Ce logiciel peut être utilisé pour un usage individuel ou en collaboration avec d\'autres.', 1);
 
 -- --------------------------------------------------------
 
@@ -54,9 +55,9 @@ INSERT INTO `tl_liens` (`lien_id`, `lien_url`, `lien_titre`, `lien_desc`, `user_
 --
 
 CREATE TABLE `tl_tags` (
-  `tag_id` int(11) NOT NULL,
-  `tag_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `tag_id` int NOT NULL,
+  `tag_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `tl_tags`
@@ -66,7 +67,9 @@ INSERT INTO `tl_tags` (`tag_id`, `tag_name`) VALUES
 (1, 'fac'),
 (2, 'vieprivee'),
 (3, 'opensource'),
-(4, 'apprendre');
+(4, 'apprendre'),
+(5, 'securite'),
+(6, 'travail');
 
 -- --------------------------------------------------------
 
@@ -75,9 +78,9 @@ INSERT INTO `tl_tags` (`tag_id`, `tag_name`) VALUES
 --
 
 CREATE TABLE `tl_tags_liens` (
-  `tag_id` int(11) NOT NULL,
-  `lien_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `tag_id` int NOT NULL,
+  `lien_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `tl_tags_liens`
@@ -87,7 +90,11 @@ INSERT INTO `tl_tags_liens` (`tag_id`, `lien_id`) VALUES
 (1, 1),
 (2, 2),
 (3, 2),
-(3, 3);
+(3, 3),
+(3, 4),
+(6, 7),
+(2, 6),
+(5, 5);
 
 -- --------------------------------------------------------
 
@@ -96,12 +103,12 @@ INSERT INTO `tl_tags_liens` (`tag_id`, `lien_id`) VALUES
 --
 
 CREATE TABLE `tl_users` (
-  `usr_id` int(11) NOT NULL,
-  `usr_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `usr_password` varchar(88) COLLATE utf8_unicode_ci NOT NULL,
-  `usr_salt` varchar(23) COLLATE utf8_unicode_ci NOT NULL,
-  `usr_role` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'ROLE_ADMIN'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `usr_id` int NOT NULL,
+  `usr_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `usr_password` varchar(88) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `usr_salt` varchar(23) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `usr_role` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'ROLE_ADMIN'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `tl_users`
@@ -140,24 +147,21 @@ ALTER TABLE `tl_users`
 -- AUTO_INCREMENT pour la table `tl_liens`
 --
 ALTER TABLE `tl_liens`
-  MODIFY `lien_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `lien_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT pour la table `tl_tags`
 --
 ALTER TABLE `tl_tags`
-  MODIFY `tag_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `tag_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `tl_users`
 --
 ALTER TABLE `tl_users`
-  MODIFY `usr_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `usr_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-grant all privileges on watson.* to 'watson'@'localhost' identified by 'watson';
